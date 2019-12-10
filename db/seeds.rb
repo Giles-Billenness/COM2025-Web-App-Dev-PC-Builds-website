f7547f43fbcb41c5ff70604d013c2f3e76ca2475
Giles-Billenness <giles.billenness@blueyonder.co.uk>
2019-12-10 20:09:27 +0000

    added welcome seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,9 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   movies = Movie.create([{ name:   'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+welcome=Pc.where(Name:'Welcome').first
+welcome.delete if welcome
+
+pcs=Pc.create([Name:'Welcome', Motherboard:'MOBO', CpuCooler:'COOLER', Case:'CASE', PowerSupply:'SUPPLY', Storage:'SSD', Price:12.3, Description: 'To the pcs section', Date:DateTime.new(2009,9,1,19)])
