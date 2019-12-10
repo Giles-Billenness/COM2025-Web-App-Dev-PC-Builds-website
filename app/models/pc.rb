2d08349b88825d17fe35231a1eed87e47159aa2a
Giles-Billenness <giles.billenness@blueyonder.co.uk>
2019-12-10 20:53:48 +0000

    pc testing

--- app/models/pc.rb
+++ app/models/pc.rb
@@ -1,2 +1,10 @@
 class Pc < ApplicationRecord
+  validates :Name, presence: true
+  validates :Motherboard, presence: true
+  validates :CpuCooler, presence: true
+  validates :Case, presence: true
+  validates :PowerSupply, presence: true
+  validates :Storage, presence: true
+  validates :Date, presence: true
+
 end
--- test/models/pc_test.rb
+++ test/models/pc_test.rb
@@ -4,4 +4,19 @@ class PcTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not save empty pc" do
+    pc=Pc.new
+    pc.save
+    refute pc.valid?
+  end
+
+
+test "should save pc" do
+  pc1=Pc.create([Name:'Welcome', Motherboard:'MOBO', CpuCooler:'COOLER', Case:'CASE', PowerSupply:'SUPPLY', Storage:'SSD', Price:12.3, Description: 'DESC', Date:DateTime.new(2009,9,1,19)])
+  pc1.save #fix this one!!!!!!!!!!!!!!!!!!!!!!!
+  assert pc1.valid?
+end
+
+
 end
