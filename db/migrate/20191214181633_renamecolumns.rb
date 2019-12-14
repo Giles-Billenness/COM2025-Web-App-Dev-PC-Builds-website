94ecb2d6007cd301ee0cb03c823016506ecdce17
Giles-Billenness <giles.billenness@blueyonder.co.uk>
2019-12-14 18:24:27 +0000

    renamed table columns

--- db/migrate/20191214181633_renamecolumns.rb
+++ db/migrate/20191214181633_renamecolumns.rb
@@ -4,6 +4,7 @@ class Renamecolumns < ActiveRecord::Migration[5.2]
     rename_column :central_processing_units, :ChipModel, :chipmodel
 
     rename_column :graphics_processing_units, :AfterMarketMake, :aftermarketmake
+    
     rename_column :central_processing_units, :ChipMake, :chipmake
     rename_column :graphics_processing_units, :ChipModel, :chipmodel
   end
