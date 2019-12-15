c2bc73da258a56037082134784012331adf3b6df
Giles-Billenness <giles.billenness@blueyonder.co.uk>
2019-12-15 17:05:26 +0000

    images working for builds

--- app/models/build.rb
+++ app/models/build.rb
@@ -1,6 +1,7 @@
 class Build < ApplicationRecord
   has_one :CentralProcessingUnit
   has_one :GraphicsProcessingUnit
+  has_one_attached :picture
 # scope :get_name -> (cpu_id) {
 #   where("cpu_id = ?", cpu_id).ChipModel
 # }
--- db/migrate/20191214202806_add_gpu_id_to_builds.rb
+++ db/migrate/20191214202806_add_gpu_id_to_builds.rb
@@ -1,5 +1,5 @@
 class AddGpuIdToBuilds < ActiveRecord::Migration[5.2]
   def change
-    add_column :builds, :gpu_id, :integer
+    add_column :builds, :gpu_id, :integer 
   end
 end
