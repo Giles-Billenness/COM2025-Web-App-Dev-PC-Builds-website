0011f343b6e7827b447b1b91862c29420e8c9b25
Giles-Billenness <giles.billenness@blueyonder.co.uk>
2019-12-16 01:28:53 +0000

    used jquery to display build info on index

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,10 @@
 // $(document).ready(function(){
 //   alert();
 // });
+$(document).ready(function() {
+    $('.image-frame').hover(function(){
+		$('.image-caption',this).slideToggle('slow');
+	}, function(){
+		$('.image-caption',this).slideToggle('slow');
+    });
+});
